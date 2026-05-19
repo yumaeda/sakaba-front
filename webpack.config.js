0ad6020a5d55cd0c1fc8071a452c0c7034777b87
Yukitaka Maeda <yumaeda@gmail.com>
2026-05-20 01:16:58 +0900

    Remove unnecessary import

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const MODE = 'production'
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { maxHeaderSize } = require('http')
 
 module.exports = [{
     mode: MODE,
